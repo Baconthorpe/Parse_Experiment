52aaa4f32f0e532390040ebe1b6c3363a419eba7
Baconthorpe <zabuhoff@gmail.com>
2014-04-08 11:31:09 -0400

    Initial Commit

---
+++ Parse_Experiment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Parse_Experiment
+//
+//  Created by Ezekiel Abuhoff on 4/8/14.
+//  Copyright (c) 2014 Ezekiel Abuhoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZAAppDelegate class]));
+    }
+}
